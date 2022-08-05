acacf4889ae4fbabb3c02ad0c9f7b5d09aea5238
Allen <allenvs@mail.ru>
2022-08-05 16:23:56 +0300

    added error handle at main function

--- struct/import/import.go
+++ struct/import/import.go
@@ -48,6 +48,9 @@ func ReadResponse(rawResp string) (Response, error) {
 
 func main() {
 	//sample := `{"header": {"code": 0,"message": ""},"data": [{"type": "user","id": 100, "attributes": {"email": "bob@yandex.ru", "article_ids": [10, 11, 12]}}]}`
-
-	fmt.Println(ReadResponse(sample))
+	res, err := ReadResponse(sample)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(res)
 }
