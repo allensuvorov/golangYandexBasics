33d941396b9b9d07b9d8dcf78e57be55e8cd8a34
Allen <allenvs@mail.ru>
2022-08-07 23:15:45 +0300

    functions for areas

--- functions/area/main.go
+++ functions/area/main.go
@@ -1,32 +1,47 @@
 package main
 
+import (
+	"math"
+)
+
 type figures int
 
-const(
-    square figures = iota // квадрат
-    circle // круг
-    triangle // равносторонний треугольник
+const (
+	square   figures = iota // квадрат
+	circle                  // круг
+	triangle                // равносторонний треугольник
 )
 
 func init() {
-	areas := make(map[figures]func(figures)float64)
-}
-
-func area(f figures)(func(float64) float64, bool) {
-	ok := f 
-	ar := func(float64) float64 {
+	areas := make(map[figures]func(float64) float64)
+	areas[square] = func(x float64) float64 {
+		return x * x
+	}
+	areas[circle] = func(x float64) float64 {
+		return x * x * math.Pi
 
 	}
-	return ar, ok
-}
-
-ar, ok := area(myFigure)
-if !ok {
-    fmt.Println("Ошибка")
-    return
-}
-myArea := ar(x)
-
-func main() {
+	areas[triangle] = func(x float64) float64 {
+		return x * x * math.Sqrt(3) / 4
+	}
 
 }
+
+// func area(f figures)(func(float64) float64, bool) {
+// 	ok := f
+// 	ar := func(float64) float64 {
+
+// 	}
+// 	return ar, ok
+// }
+
+// ar, ok := area(myFigure)
+// if !ok {
+//     fmt.Println("Ошибка")
+//     return
+// }
+// myArea := ar(x)
+
+// func main() {
+
+// }
