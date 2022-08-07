012bd0f60b0e5b77a38ffa6e8c925d0578bc104a
Allen <allenvs@mail.ru>
2022-08-07 17:53:24 +0300

    NewItem - initial values

--- functions/ItemWithOptions/main.go
+++ functions/ItemWithOptions/main.go
@@ -11,5 +11,9 @@ type option func(*Item)
 
 // constructor with options
 func NewItem(opts ...option) *Item {
-
+	i := &Item{
+		NoOption:   "usual",
+		Parameter1: "default",
+		Parameter2: 42,
+	}
 }
