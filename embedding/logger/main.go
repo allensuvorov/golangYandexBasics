5dd78c9cd4e0bb62294eb3975aaf53b3db71efd8
Allen <allenvs@mail.ru>
2022-08-15 22:53:55 +0300

    LogLevel type, enum and string

--- embedding/logger/main.go
+++ embedding/logger/main.go
@@ -2,7 +2,31 @@ package main
 
 import "log"
 
+// package log
+// type Logger (struct)
+// method log.Println, log.Printf
+//--------
+
 // func New(out io.Writer, prefix string, flag int) *Logger
+type LogLevel int
+
+const (
+	Info LogLevel = iota
+	Warning
+	Error
+)
+
+func (l LogLevel) String() string {
+	switch l {
+	case Info:
+		return ""
+	case Warning:
+		return "WARN"
+	case Error:
+		return "ERR"
+	}
+	return ""
+}
 
 type LogExtended struct {
 	*log.Logger
@@ -10,6 +34,7 @@ type LogExtended struct {
 }
 
 func main() {
+	// log.Print("Log it baby!")
 	logger := NewLogExtended()
 	logger.SetLogLevel(LogLevelWarning)
 	logger.Infoln("Не должно напечататься")
