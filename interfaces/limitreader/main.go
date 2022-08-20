5fd4c64c2e148710e0caa0398efe360dc4f69ad8
Allen <allenvs@mail.ru>
2022-08-20 22:26:07 +0300

    limitreader works

--- interfaces/limitreader/main.go
+++ interfaces/limitreader/main.go
@@ -9,10 +9,16 @@ import (
 
 func LimitReader(r io.Reader, n int) io.Reader {
 	// ...
+	b := make([]byte, n)
+	r.Read(b)
+	return strings.NewReader(string(b) + "\n")
+
 }
 
 func main() {
+	// new struct with string and Reader method inside
 	r := strings.NewReader("some io.Reader stream to be read\n")
+	// call LimitReader function for the Reader struct and limit
 	lr := LimitReader(r, 4)
 
 	_, err := io.Copy(os.Stdout, lr)
