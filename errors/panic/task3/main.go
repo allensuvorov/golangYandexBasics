bd106956b2fcef43c97855941944c0afeef8d760
Allen <allenvs@mail.ru>
2022-09-07 15:28:28 +0300

    panic task3 retyped solution, second look

--- errors/panic/task3/main.go
+++ errors/panic/task3/main.go
@@ -9,10 +9,24 @@ type input struct {
 }
 
 // 1) добавьте функцию f
+func f(a, b, counter int) {
+	if counter == 0 {
+		panic(`counter equals 0`)
+	}
+	f(b, a/b, counter-1)
+}
 
 // 2) добавьте функцию test(a, b, counter int) (err error),
 // которая преобразует панику в ошибку
-// ...
+func test(a, b, counter int) (err error) {
+	defer func() {
+		if p := recover(); p != nil {
+			err = fmt.Errorf("%v", p)
+		}
+	}()
+	f(a, b, counter)
+	return nil
+}
 
 func main() {
 	for _, pars := range []input{
