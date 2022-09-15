95c4b424d5ee180327e00c46e024f96c0af0a66e
Allen <allenvs@mail.ru>
2022-09-15 18:28:10 +0300

    added task code

---
+++ test/task4/main.go
@@ -0,0 +1,21 @@
+package task4
+
+import "fmt"
+
+func EstimateValue(value int) string {
+	switch {
+	case value < 10:
+		return "small"
+
+	case value < 100:
+		return "medium"
+
+	default:
+		return "big"
+	}
+}
+
+func main() {
+	v := 10
+	fmt.Println(EstimateValue(v))
+}
